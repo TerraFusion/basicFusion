42f054a29638a5737241280f2d8ef6b25bcd3b8d
LandonTClipp <clipp2@illinois.edu>
2017-06-06 14:30:30 -0500

    Updating database generation/query scripts

--- metadata-input/build/generateDB.sh
+++ metadata-input/build/generateDB.sh
@@ -3,9 +3,10 @@ export PYTHONUSERBASE=/home/clipp/libs/python
 export PYTHONPATH=$PYTHONUSERBASE/lib/python2.7/site-packages:$PYTHONPATH
 module load python/2.7.10
 
+DATADIR="../data"
 ./ROGER-findFiles
 for i in ASTER MODIS MISR MOPITT CERES; do
-   gzip -f data/$i.list 
+   gzip -f "$DATADIR"/$i.list 
 done
 
-python fusionBuildDB -o -q --discards=discards.txt --anomalies=errors.txt accesslist.sqlite data/Orbit_Path_Time.txt.gz data/MODIS.list.gz data/ASTER.list.gz data/MOPITT.list.gz data/MISR.list.gz data/CERES.list.gz
+python ./fusionBuildDB -o -q --discards=discards.txt --anomalies=errors.txt ../accesslist.sqlite "$DATADIR"/Orbit_Path_Time.txt.gz "$DATADIR"/MODIS.list.gz "$DATADIR"/ASTER.list.gz "$DATADIR"/MOPITT.list.gz "$DATADIR"/MISR.list.gz "$DATADIR"/CERES.list.gz
