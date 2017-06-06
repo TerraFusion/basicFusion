d2efa442856976ab7ca459035e417d6829c552d8
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2017-06-06 09:41:13 -0500

    fix for #120.

--- CMR/ingest.sh
+++ CMR/ingest.sh
@@ -6,3 +6,8 @@ curl -v -XPOST -H "Content-Type: application/echo10+xml" -H "Accept: application
 
 # Test granule validation.
 curl -v -XPOST -H "Content-Type: application/echo10+xml" -H "Accept: application/xml" -H "Echo-Token: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" -d @InputGranules.xml https://cmr.uat.earthdata.nasa.gov/ingest/providers/FUSION/validate/granule/InputGranules
+
+# Validate locally.
+# https://raw.githubusercontent.com/nasa/Common-Metadata-Repository/8b81dbeb8ed08418cb9dcaec53cd3487a96a85da/umm-lib/resources/schema/echo10/Granule.xsd
+# https://raw.githubusercontent.com/nasa/Common-Metadata-Repository/8b81dbeb8ed08418cb9dcaec53cd3487a96a85da/umm-lib/resources/schema/echo10/MetadataCommon.xsd
+xmllint --schema Granule.xsd --noout InputGranules_generated.xml
