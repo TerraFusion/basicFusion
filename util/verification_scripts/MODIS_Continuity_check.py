8e8cf1689672c5823a6db52abab8c8815dad0e71
Shashank <sbansal6@illinois.edu>
2017-07-14 13:58:03 -0500

    Update MODIS_Continuity_check.py

--- util/verification_scripts/MODIS_Continuity_check.py
+++ util/verification_scripts/MODIS_Continuity_check.py
@@ -4,117 +4,116 @@
 
 """
 
-from os import path
 import os
-import fnmatch
 from collections import defaultdict
 import collections
-import mmap
-from datetime import datetime, timedelta
+import sys
 
 def main():
+	if not len(sys.argv) > 1:
+		usage()
 
-	dirPath2000 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2000'
-	filename2000 = "MODIS_Continuity_Check_MOD021KM_2000.txt"
-	
-	dirPath2001 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2001'
-	filename2001 = "MODIS_Continuity_Check_MOD021KM_2001.txt"
-	
-	dirPath2002 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2002'
-	filename2002 = "MODIS_Continuity_Check_MOD021KM_2002.txt"
-	
-	dirPath2003 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2003'
-	filename2003 = "MODIS_Continuity_Check_MOD021KM_2003.txt"
-	
-	dirPath2004 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2004'
-	filename2004 = "MODIS_Continuity_Check_MOD021KM_2004.txt"
-	
-	dirPath2005 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2005'
-	filename2005 = "MODIS_Continuity_Check_MOD021KM_2005.txt"
-	
-	dirPath2006 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2006'
-	filename2006 = "MODIS_Continuity_Check_MOD021KM_2006.txt"
-	
-	dirPath2007 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2007'
-	filename2007 = "MODIS_Continuity_Check_MOD021KM_2007.txt"
-	
-	dirPath2008 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2008'
-	filename2008 = "MODIS_Continuity_Check_MOD021KM_2008.txt"
-	
-	dirPath2009 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2009'
-	filename2009 = "MODIS_Continuity_Check_MOD021KM_2009.txt"
-	
-	dirPath2010 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2010'
-	filename2010 = "MODIS_Continuity_Check_MOD021KM_2010.txt"
-	
-	dirPath2011 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2011'
-	filename2011 = 'MODIS_Continuity_Check_MOD021KM_2011.txt'
-	
-	dirPath2012 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2012'
-	filename2012 = 'MODIS_Continuity_Check_MOD021KM_2012.txt'
-	
-	dirPath2013 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2013'
-	filename2013 = 'MODIS_Continuity_Check_MOD021KM_2013.txt'
-	
-	dirPath2014 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2014'
-	filename2014 = 'MODIS_Continuity_Check_MOD021KM_2014.txt'
-	
-	dirPath2015 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2015'
-	filename2015 = 'MODIS_Continuity_Check_MOD021KM_2015.txt'
-	
-	dirPath2016 = '/projects/TDataFus/gyzhao/TF/data/MODIS/MOD021KM/2016'
-	filename2016 = 'MODIS_Continuity_Check_MOD021KM_2016.txt'
-	
+	elif sys.argv[1] == '-h' or sys.argv[1] == '-help' :
+		help()
 
-	check_Continuity(dirPath2000, filename2000, 367)
-	check_Continuity(dirPath2001, filename2001, 366)
-	check_Continuity(dirPath2002, filename2002, 366)
-	check_Continuity(dirPath2003, filename2003, 366)
-	check_Continuity(dirPath2004, filename2004, 367)
-	check_Continuity(dirPath2005, filename2005, 366)
-	check_Continuity(dirPath2006, filename2006, 366)
-	check_Continuity(dirPath2007, filename2007, 366)
-	check_Continuity(dirPath2008, filename2008, 367)
-	check_Continuity(dirPath2009, filename2009, 366)
-	check_Continuity(dirPath2010, filename2010, 366)
-	check_Continuity(dirPath2011, filename2011, 366)
-	check_Continuity(dirPath2012, filename2012, 367)
-	check_Continuity(dirPath2013, filename2013, 366)
-	check_Continuity(dirPath2014, filename2014, 366)
-	check_Continuity(dirPath2015, filename2015, 366)
-	check_Continuity(dirPath2016, filename2016, 367)
+	elif sys.argv[1] == '-a':
+		arguments()
+
+	elif sys.argv[1] == '-r':
+		if len(sys.argv) != 6:
+			arguments()
+
+		else:
+			dirpath = sys.argv[2]
+			logfile = sys.argv[3]
+			year = sys.argv[4]
+			dateTime = sys.argv[5]
+			run(dirpath, logfile, year, dateTime)
+	else:
+		usage()
+
+def usage():
+	print("\n\nUSAGE:  python MODIS_Continuity_Check.py [-h | -a | -r]\n")
+	print("Options:\n")
+	print("-h 		    					More information about the different functions and how they work\n")
+	print("-a           						More information about the arguments and format\n")
+	print("-r [dirpath] [logfile] [year] [dateTime] 		Run the program\n")
 
 
-def check_Continuity(dirPath, textfile, leap):
+def help():
+	print("\nThere is one main function in this program.\n")
+
+	print("1. Continuity_Check(dirpath, logfile, year, dateTime): \nThis function is used to check if their is any discontinuity/missing files. The way a file is logged is, ")
+	print("if there is gap of more than the interval of 5 minutes between 2 files. For example, Lets say that following")
+	print("are some of the files in the directory, /projects/TDataFus/gyzhao/TF/data/MODIS/MOD03/2015/056\n")
+	print("MOD03.A2015056.1340.006")
+	print("MOD03.A2015056.1350.006")
+	print("MOD03.A2015056.1355.006")
+	print("\nHere MOD03.A2015056.1345.006 is missing file. In MODIS, data is collected at an interval of every 5 minutes starting from")
+	print("0000 to 2355.\n")
+
+def arguments():
+	print("\nARGUMENTS TO RUN PROGRAM:\n\n python MODIS_Continuity_Check.py -r [absolute/path/to/dir] [filepath/to/logfile] [year] [path/to/dateTime.txt]\n")
+	print("For more information on each argument, type i.")
+	info = str(raw_input())
+
+	if(info == 'i' or info =='I'):
+		print("MORE INFORMATION:\n\nFor this program to run properly, you need 4 different arguments.")
+		print("1. The first is the directory path. For example, it should look something like:")
+		print("\npath/to/dir or /projects/TDataFus/gyzhao/TF/data/MODIS/MOD03\n")
+		print("In order for the program to run smoothly, you need to provide the absolute path and make sure that you")
+		print("provide the dirpath only uptil just before the year.\n")
+		
+		print("2. The second argument is the file path to the logfile where you wish to write the names of the missing files.")
+		print("For example, it should look something like,")
+		print("\n/path/to/logfile/logfile.txt or /home/user/scratch/BasicFusion/MODIS_Cont_Check_logfiles/logfile.txt\n")
+		print("The second argument also need to have the name of the .txt file where you wish to log all the missing files.")
+		print("You don't need to make a TextFile, just give the name in the second argument and the program will make a txtfle")
+		print("with that name in the specified directory.\n")
+		
+		print("3. The third argument is the year you wish to run the program on.\n")
+		
+		print("4. The fourth argument is the path to the dateTime file. It should be a txt file that contains all the times in")
+		print("the day at an interval of 5 minutes starting from 0000 to 2355. For example, ")
+		print("\npath/to/orbFile or /home/user/File_Verification_Scripts/scripts/dateTime.txt\n")
+
+def run(dirpath, logfile, year, datetime):
+	Continuity_Check(dirpath, logfile, year, datetime)
+	
+
+
+def Continuity_Check(dirpath, textfile, year, dateTime):
 	exist = {}
 	orbitFiles = defaultdict(list)
+	leap = [2000, 2004, 2008, 2012, 2016, 2020, 2024]
 
 	checklist = []
 	numlist = []
 	emptydir = []
 	hdfempty = []
 
-
-	for i in range(1, leap):
+	#makes a list of all the days in the year
+	for i in range(1, 367):
 		checklist.append(i)
 
 	#this makes a list of all the dates than need to be checked
-	with open("datetime.txt") as file:
+	with open(dateTime) as file:
 		for line in file:
 			numlist.append(line[0:4])
 
 	#this is the text file where all the data is written 
 	fileCheck = open(textfile, "w").close()
 	fileCheck = open(textfile, "w")
-	
 
+	#changing into the year directory
+	dirPath = os.path.join(dirpath, year)
+	
 	for fname in os.listdir(dirPath):
 
 		#checks whether the directory in the particular year is empty
 		if(os.listdir(os.path.join(dirPath, fname)) == []):
 			emptydir.append(fname)
 			checklist.remove(int(fname))
-
 
 		else:
 			checklist.remove(int(fname))
@@ -148,6 +147,9 @@ def check_Continuity(dirPath, textfile, leap):
 
 					fileCheck.write('\n')
 
+	#removes the extra day if its not a leap year
+	if year not in leap:
+		checklist.remove('366')
 	
 	#writes to the text file if and day is missing from the year
 	if missinglist:
@@ -172,4 +174,3 @@ def check_Continuity(dirPath, textfile, leap):
 			fileCheck.write(hdfempty[i] + '\n')
 
 main()
-
