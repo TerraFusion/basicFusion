a57b318c5e66fa8fe846f4d097dbd663ffdf640a
LandonTClipp <clipp2@illinois.edu>
2017-10-10 20:44:01 -0500

    Changed xticks on matplotlib plot

--- util/verification_scripts/inquireSize.py
+++ util/verification_scripts/inquireSize.py
@@ -56,8 +56,11 @@ sizes and outputs 8 histogram plots where the x-axis is file size and the y-axis
     figNames = [ ['mop.png', 'MOPITT'], ['cer.png', 'CERES'], ['mod.png', 'MODIS'], ['ast.png', 'ASTER'], \
                  ['mis_grp.png', 'MISR GRP'], ['mis_agp.png', 'MISR AGP'], ['mis_gmp.png', 'MISR GMP'], \
                  ['mis_hrll.png', 'MISR HRLL'] ]
+    
+    bins=range(0,300,20)
     for i in xrange(0, 8):
-        plt.hist( instrSizes[i] )
+        plt.hist( instrSizes[i], bins=bins)
+        plt.xticks( bins )
         plt.title("File size frequency (" + figNames[i][1] + ')')
         plt.xlabel("Size (MB)")
         plt.ylabel("Frequency")
